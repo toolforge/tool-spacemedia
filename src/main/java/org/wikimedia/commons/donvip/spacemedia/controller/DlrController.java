d19e252dd9e9e176a6b686120e8494ff9f294dbe
Vincent Privat <vincent.privat@gmail.com>
2019-08-11 21:01:52 +0200

    add DLR controller
    Former-commit-id: d029c1e8ebce1a256832031c2ca5b0bc4fe19203

---
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/DlrController.java
@@ -0,0 +1,39 @@
+package org.wikimedia.commons.donvip.spacemedia.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wikimedia.commons.donvip.spacemedia.data.local.flickr.FlickrMedia;
+import org.wikimedia.commons.donvip.spacemedia.service.agencies.DlrService;
+
+@RestController()
+@RequestMapping("/spacemedia/dlr")
+public class DlrController {
+
+    @Autowired
+    private DlrService service;
+
+    @GetMapping("/all")
+    public Iterable<? extends FlickrMedia> listAll() throws IOException {
+        return service.listAllMedia();
+    }
+
+    @GetMapping("/update")
+    public List<FlickrMedia> update() throws IOException {
+        return service.updateMedia();
+    }
+
+    @GetMapping("/missing")
+    public List<FlickrMedia> listMissing() throws IOException {
+        return service.listMissingMedia();
+    }
+
+    @GetMapping("/duplicates")
+    public List<FlickrMedia> listDuplicate() throws IOException {
+        return service.listDuplicateMedia();
+    }
+}
