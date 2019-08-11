f4b65990bec9e9380e52787a0a02b04812491217
Vincent Privat <vincent.privat@gmail.com>
2019-08-11 21:02:10 +0200

    add NASA (Flickr)
    Former-commit-id: 5f315cf87a0fd6fab48d1a440f4727fe7ff4f541

---
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/agencies/NasaFlickrService.java
@@ -0,0 +1,38 @@
+package org.wikimedia.commons.donvip.spacemedia.service.agencies;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.wikimedia.commons.donvip.spacemedia.data.local.ProblemRepository;
+import org.wikimedia.commons.donvip.spacemedia.data.local.flickr.FlickrMedia;
+import org.wikimedia.commons.donvip.spacemedia.data.local.flickr.FlickrMediaRepository;
+import org.wikimedia.commons.donvip.spacemedia.service.FlickrService;
+import org.wikimedia.commons.donvip.spacemedia.service.MediaService;
+
+import com.github.dozermapper.core.Mapper;
+
+@Service
+public class NasaFlickrService extends SpaceAgencyFlickrService {
+
+    @Autowired
+    public NasaFlickrService(FlickrMediaRepository repository, ProblemRepository problemrepository,
+            MediaService mediaService, FlickrService flickrService,
+            Mapper dozerMapper, @Value("${nasa.flickr.accounts}") Set<String> flickrAccounts) {
+        super(repository, problemrepository, mediaService, flickrService, dozerMapper, flickrAccounts);
+    }
+
+    @Override
+    @Scheduled(fixedRateString = "${nasa.flickr.update.rate}")
+    public List<FlickrMedia> updateMedia() {
+        return updateFlickrMedia();
+    }
+
+    @Override
+    public String getName() {
+        return "Nasa (Flickr)";
+    }
+}
