743ea688eb9c215342e6e3d21ae489138d25b819
Vincent Privat <vincent.privat@gmail.com>
2019-08-11 23:13:04 +0200

    fix NasaController
    Former-commit-id: 95c475b7f1010b01a64e2a9aba15a43efa9f8960

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/NasaController.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/NasaController.java
@@ -1,10 +1,6 @@
 package org.wikimedia.commons.donvip.spacemedia.controller;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wikimedia.commons.donvip.spacemedia.data.local.nasa.NasaMedia;
@@ -12,28 +8,10 @@ import org.wikimedia.commons.donvip.spacemedia.service.agencies.NasaService;
 
 @RestController()
 @RequestMapping("/spacemedia/nasa")
-public class NasaController {
+public class NasaController extends SpaceAgencyController<NasaMedia, String> {
 
     @Autowired
-    private NasaService service;
-
-    @GetMapping("/all")
-    public Iterable<? extends NasaMedia> listAll() throws IOException {
-        return service.listAllMedia();
-    }
-
-    @GetMapping("/update")
-    public List<NasaMedia> update() throws IOException {
-        return service.updateMedia();
-    }
-
-    @GetMapping("/missing")
-    public List<NasaMedia> listMissing() throws IOException {
-        return service.listMissingMedia();
-    }
-
-    @GetMapping("/duplicates")
-    public List<NasaMedia> listDuplicate() throws IOException {
-        return service.listDuplicateMedia();
+    public NasaController(NasaService service) {
+        super(service);
     }
 }
