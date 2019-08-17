50fbfd6967dfc234c804798470a7c046e0a44a0b
Vincent Privat <vincent.privat@gmail.com>
2019-08-17 13:43:31 +0200

    T71088 - add note about major performance bug

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/commons/CommonsFileArchiveRepository.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/commons/CommonsFileArchiveRepository.java
@@ -6,5 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CommonsFileArchiveRepository extends CrudRepository<CommonsFileArchive, Integer> {
 
+    /**
+     * Retrieves deleted files by their base36 SHA-1. This method suffers
+     * a major performance bug on the database replica (the field is no indexed,
+     * see <a href="https://phabricator.wikimedia.org/T71088">#T71088</a>
+     * 
+     * @param sha1 base36 SHA-1, zero padded to 31 characters
+     * @return list of deleted files matching the provided hash
+     */
     List<CommonsFileArchive> findBySha1(String sha1);
 }
