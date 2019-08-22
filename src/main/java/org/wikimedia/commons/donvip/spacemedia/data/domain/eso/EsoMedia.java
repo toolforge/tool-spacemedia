7fc5a5d0cb429ddb13bdaad3e07c8a44105ac06e
Vincent Privat <vincent.privat@gmail.com>
2019-08-23 00:31:28 +0200

    increase ESO credit field length

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/eso/EsoMedia.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/eso/EsoMedia.java
@@ -71,7 +71,7 @@ public class EsoMedia extends FullResMedia {
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> categories;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, length = 300)
     private String credit;
 
     public String getId() {
