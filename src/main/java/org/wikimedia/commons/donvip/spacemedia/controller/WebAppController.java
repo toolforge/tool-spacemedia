feb0fcd449ff652427edc2ff146f8b0d135730b1
Vincent Privat <vincent.privat@gmail.com>
2019-08-25 01:43:15 +0200

    fix maven build failure

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/WebAppController.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/WebAppController.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.Media;
 import org.wikimedia.commons.donvip.spacemedia.service.agencies.AbstractSpaceAgencyService;
 
 @Controller
 public class WebAppController {
 
     @Autowired
-    private List<AbstractSpaceAgencyService<?, ?>> agencies;
+    private List<AbstractSpaceAgencyService<? extends Media, ?>> agencies;
 
     @GetMapping("/")
     public String index(Model model) {
