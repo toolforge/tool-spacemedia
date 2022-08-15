459c4547c02664aa3cd1261cb55a10135f2d4a0d
Vincent Privat <vincent.privat@gmail.com>
2022-08-16 01:34:42 +0200

    DVIDS: catch DAO errors

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/service/agencies/AbstractAgencyDvidsService.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/agencies/AbstractAgencyDvidsService.java
@@ -28,6 +28,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -247,6 +248,8 @@ public abstract class AbstractAgencyDvidsService<OT extends Media<OID, OD>, OID,
                 LOGGER.error("I/O error while processing DVIDS {} from unit {}: {}", id, unit, smartExceptionLog(e));
             } catch (UploadException e) {
                 LOGGER.error("Upload error while processing DVIDS {} from unit {}: {}", id, unit, smartExceptionLog(e));
+            } catch (DataAccessException e) {
+                LOGGER.error("DAO error while processing DVIDS {} from unit {}: {}", id, unit, smartExceptionLog(e));
             }
         }
         ApiPageInfo pi = response.getPageInfo();
