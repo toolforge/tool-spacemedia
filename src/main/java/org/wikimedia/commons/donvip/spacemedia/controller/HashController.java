df7a3ee0749dfe5da5c0ef576d24a3fb5db62b4d
Vincent Privat <vincent.privat@gmail.com>
2022-08-21 23:54:41 +0200

    New REST enpoint to put a new hash association

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/HashController.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/HashController.java
@@ -2,17 +2,29 @@ package org.wikimedia.commons.donvip.spacemedia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.HashAssociation;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.HashAssociationRepository;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.RuntimeDataRepository;
 
 @RestController
 public class HashController {
 
     @Autowired
-    private RuntimeDataRepository runtime;
+    private RuntimeDataRepository runtimeRepo;
+
+    @Autowired
+    private HashAssociationRepository hashRepo;
 
     @GetMapping("/hashLastTimestamp")
     public String hashLastTimestamp() {
-        return runtime.findById("commons").orElseThrow().getLastTimestamp();
+        return runtimeRepo.findById("commons").orElseThrow().getLastTimestamp();
+    }
+
+    @PutMapping("/hashAssociation")
+    public HashAssociation putHashAssociation(@RequestBody HashAssociation association) {
+        return hashRepo.save(association);
     }
 }
