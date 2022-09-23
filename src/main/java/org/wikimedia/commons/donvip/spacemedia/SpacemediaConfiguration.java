118d2bc136d8010eac3db2e5412fcea2a573fa8f
Vincent Privat <vincent.privat@gmail.com>
2022-09-24 00:49:42 +0200

    Use JPEG plugin from twelve-monkeys in order to read more files

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/SpacemediaConfiguration.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/SpacemediaConfiguration.java
@@ -7,11 +7,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.twelvemonkeys.servlet.image.IIOProviderContextListener;
+
 @Configuration
 public class SpacemediaConfiguration {
 
     @Bean(destroyMethod = "shutdown")
     public ExecutorService taskExecutor(@Value("${threads.number:8}") int threadsNumber) {
         return Executors.newScheduledThreadPool(threadsNumber);
+    }
+
+    @Bean
+    public IIOProviderContextListener iioProviderContextListener() {
+        return new IIOProviderContextListener();
     }
 }
