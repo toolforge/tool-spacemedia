8d973379deace4fda8f6ae6b5be8a5de3e638084
Vincent Privat <vincent.privat@gmail.com>
2022-12-15 21:19:24 +0100

    Handle tool restarts

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/service/RemoteService.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/RemoteService.java
@@ -2,28 +2,52 @@ package org.wikimedia.commons.donvip.spacemedia.service;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException.NotFound;
+import org.springframework.web.client.HttpServerErrorException.BadGateway;
+import org.springframework.web.client.HttpServerErrorException.ServiceUnavailable;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.HashAssociation;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.Media;
 
 @Service
 public class RemoteService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemoteService.class);
+
     @Value("${remote.application.uri}")
     private URI remoteApplication;
 
     @Autowired
     private RestTemplate restTemplate;
 
+
     public String getHashLastTimestamp() {
         return restTemplate.getForObject(remoteApplication + "/hashLastTimestamp", String.class);
     }
 
     public void putHashAssociation(HashAssociation hash) {
-        restTemplate.put(remoteApplication + "/hashAssociation", hash);
+        boolean ok = false;
+        do {
+            try {
+                restTemplate.put(remoteApplication + "/hashAssociation", hash);
+                ok = true;
+            } catch (BadGateway | ServiceUnavailable | ResourceAccessException e) {
+                // Tool is restarting... loop until it comes back
+                LOGGER.debug("{}", e.getMessage(), e);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    LOGGER.error("{}", ex.getMessage(), ex);
+                    Thread.currentThread().interrupt();
+                }
+            }
+        } while (!ok);
     }
 
     public void saveMedia(String agencyId, Media<?, ?> media) {
