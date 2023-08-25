1c4ee208e149aa808b6d8e762cde4c28db7d430a
Vincent Privat <vincent.privat@gmail.com>
2023-08-25 21:19:26 +0200

    Umbra: add SDC metdata

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/UmbraS3Service.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/UmbraS3Service.java
@@ -1,13 +1,19 @@
 package org.wikimedia.commons.donvip.spacemedia.service.orgs;
 
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +23,7 @@ import org.wikimedia.commons.donvip.spacemedia.data.domain.base.FileMetadata;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.s3.S3Media;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.s3.S3MediaId;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.s3.S3MediaRepository;
+import org.wikimedia.commons.donvip.spacemedia.service.GeometryService;
 import org.wikimedia.commons.donvip.spacemedia.service.orgs.UmbraS3Service.UmbraMetadata.Collect;
 import org.wikimedia.commons.donvip.spacemedia.utils.Emojis;
 
@@ -31,8 +38,16 @@ public class UmbraS3Service extends AbstractOrgS3Service {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UmbraS3Service.class);
 
+    private static final Map<String, String> SATS = Map.of("01", "Q121841901", "02", "Q121841939", "03", "Q121841944",
+            "04", "Q121841948", "05", "Q121841953", "06", "Q121841955", "07", "Q121842030", "08", "Q121842041");
+
+    private static final Pattern SAT_PATTERN = Pattern.compile(".*_UMBRA-(\\d{2})/.*");
+
     @Autowired
     private ObjectMapper jackson;
+
+    @Autowired
+    private GeometryService geometry;
 
     @Autowired
     public UmbraS3Service(S3MediaRepository repository,
@@ -50,8 +65,14 @@ public class UmbraS3Service extends AbstractOrgS3Service {
     protected S3Media enrichS3Media(S3Media media) throws IOException {
         S3MediaId id = media.getId();
         String[] items = id.getObjectKey().split("/");
-        if (items.length == 6) {
+        if (items.length == 5) {
+            media.setTitle(items[3]);
+        }else if (items.length == 6) {
             media.setTitle(items[2] + " (" + items[4] + ')');
+        } else if (items.length == 7) {
+            media.setTitle(items[3] + " (" + items[5] + ')');
+        } else if (items.length == 8) {
+            media.setTitle(items[4] + " (" + items[6] + ", " + items[3].split(" ")[0] + ')');
         } else if (items.length == 9) {
             media.setTitle(items[7] + " (" + items[8].replace(".tif", "") + ')');
         } else {
@@ -61,12 +82,17 @@ public class UmbraS3Service extends AbstractOrgS3Service {
         try (InputStream in = getS3Object(id.getBucketName(),
                 id.getObjectKey().replace("GEC.tif", "METADATA.json").replace(".tif", "_METADATA.json"))
                 .getObjectContent()) {
-            Collect collect = jackson.readValue(in, UmbraMetadata.class).getCollects().get(0);
-            List<Double> center = collect.getSceneCenterPointLla().getCoordinates();
-            media.setCreationDateTime(collect.getStartAtUTC()
-                    .plus(Duration.between(collect.getStartAtUTC(), collect.getEndAtUTC()).dividedBy(2)));
-            media.setLongitude(center.get(0));
-            media.setLatitude(center.get(1));
+            List<Collect> collects = jackson.readValue(in, UmbraMetadata.class).getCollects();
+            if (isNotEmpty(collects)) {
+                Collect collect = collects.get(0);
+                List<Double> center = collect.getSceneCenterPointLla().getCoordinates();
+                media.setCreationDateTime(collect.getStartAtUTC()
+                        .plus(Duration.between(collect.getStartAtUTC(), collect.getEndAtUTC()).dividedBy(2)));
+                media.setLongitude(center.get(0));
+                media.setLatitude(center.get(1));
+            } else {
+                LOGGER.warn("No collect for {}", id);
+            }
         }
         return media;
     }
@@ -74,7 +100,8 @@ public class UmbraS3Service extends AbstractOrgS3Service {
     @Override
     public Set<String> findCategories(S3Media media, FileMetadata metadata, boolean includeHidden) {
         Set<String> result = super.findCategories(media, metadata, includeHidden);
-        result.add("Images by Umbra");
+        String continent = geometry.getContinent(media.getLatitude(), media.getLongitude());
+        result.add(continent != null ? "Images of " + continent + " by Umbra" : "Images by Umbra");
         return result;
     }
 
@@ -90,6 +117,19 @@ public class UmbraS3Service extends AbstractOrgS3Service {
         // https://registry.opendata.aws/umbra-open-data/
         // https://umbra.space/terms-of-use
         return "Umbra Lab, Inc.";
+    }
+
+    @Override
+    protected Map<String, Pair<Object, Map<String, Object>>> getStatements(S3Media media, FileMetadata metadata) {
+        Map<String, Pair<Object, Map<String, Object>>> result = super.getStatements(media, metadata);
+        Matcher m = SAT_PATTERN.matcher(media.getId().getObjectKey());
+        if (m.matches()) {
+            result.put("P170", Pair.of(SATS.get(m.group(1)), null)); // Created by UMBRA-XX
+        }
+        result.put("P1071", Pair.of("Q663611", null)); // Created in low earth orbit
+        result.put("P2079", Pair.of("Q725252", null)); // Satellite imagery
+        result.put("P4082", Pair.of("Q740686", null)); // Taken with SAR
+        return result;
     }
 
     @Override
