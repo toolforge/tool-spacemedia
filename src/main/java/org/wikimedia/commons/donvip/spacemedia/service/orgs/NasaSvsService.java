c3e1170a2f890af359152398ad05fa468564f3df
Vincent Privat <vincent.privat@gmail.com>
2024-02-24 14:33:51 +0100

    NASA SVS: ignore  Time-stamp slates

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/NasaSvsService.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/NasaSvsService.java
@@ -157,6 +157,7 @@ public class NasaSvsService extends AbstractOrgService<NasaSvsMedia> {
                 group.mediaItemsStream()
                         .filter(x -> x.media_type().shouldBeOkForCommons()
                                 && !x.url().toExternalForm().endsWith(".exr")
+                                && !x.alt_text().contains("stamp slate")
                                 && !x.alt_text().startsWith("Time slates for the multiple movies")
                                 && !x.alt_text().startsWith("This timeline is synchronized with"))
                         .sorted(comparingLong(NasaSvsMediaItem::pixels).reversed()).findFirst().ifPresent(biggest -> {
