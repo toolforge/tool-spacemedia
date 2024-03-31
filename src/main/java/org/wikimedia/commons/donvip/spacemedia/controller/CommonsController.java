a73950ceae29bf7ad1e7e44603a6ed03883050ed
Vincent Privat <vincent.privat@gmail.com>
2024-03-31 20:14:52 +0200

    Video2Commons: new endpoint to refresh task states

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/CommonsController.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/controller/CommonsController.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wikimedia.commons.donvip.spacemedia.service.wikimedia.CommonsService;
+import org.wikimedia.commons.donvip.spacemedia.service.wikimedia.Video2CommonsService;
 
 @RestController
 public class CommonsController {
@@ -15,9 +16,18 @@ public class CommonsController {
     @Autowired
     private CommonsService commons;
 
+    @Lazy
+    @Autowired
+    private Video2CommonsService v2c;
+
     @GetMapping("/checkDuplicates")
     public void checkDuplicates() throws IOException {
         commons.checkExactDuplicateFiles();
+    }
+
+    @GetMapping("/checkVideos2Commons")
+    public void checkVideos2Commons() throws IOException {
+        v2c.checkTasks();
     }
 
     @GetMapping("/computeHashesAsc")
