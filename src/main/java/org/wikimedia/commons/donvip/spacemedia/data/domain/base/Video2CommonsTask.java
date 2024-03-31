3ba8eb895844cd63373403b26682c318b54350f9
Vincent Privat <vincent.privat@gmail.com>
2024-03-31 18:43:47 +0200

    Video2Commons: track created tasks

---
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/base/Video2CommonsTask.java
@@ -0,0 +1,127 @@
+package org.wikimedia.commons.donvip.spacemedia.data.domain.base;
+
+import static java.util.Objects.requireNonNull;
+
+import java.net.URL;
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+
+@Entity
+public class Video2CommonsTask {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false, length = 540)
+    private URL url;
+
+    @Column(nullable = false, length = 255)
+    private String filename;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true, length = 8)
+    private Status status;
+
+    private int progress;
+
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @Column(nullable = true)
+    private ZonedDateTime lastChecked;
+
+    public enum Status {
+        PENDING, PROGRESS, SUCCESS, FAILURE, RETRY, ABORTED;
+
+        public boolean shouldSucceed() {
+            return this == PENDING || this == PROGRESS || this == SUCCESS || this == RETRY;
+        }
+    }
+
+    public Video2CommonsTask() {
+
+    }
+
+    public Video2CommonsTask(String id, URL url, String filename) {
+        setId(requireNonNull(id));
+        setUrl(requireNonNull(url));
+        setFilename(requireNonNull(filename));
+        setCreated(ZonedDateTime.now());
+        setProgress(-1);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setStatus(String status) {
+        this.status = Status.valueOf(status);
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public ZonedDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(ZonedDateTime created) {
+        this.created = created;
+    }
+
+    public ZonedDateTime getLastChecked() {
+        return lastChecked;
+    }
+
+    public void setLastChecked(ZonedDateTime lastChecked) {
+        this.lastChecked = lastChecked;
+    }
+
+    @Override
+    public String toString() {
+        return "Video2CommonsTask [" + (id != null ? "id=" + id + ", " : "")
+                + (filename != null ? "filename=" + filename + ", " : "")
+                + (status != null ? "status=" + status + ", " : "")
+                + "progress=" + progress + ", "
+                + (created != null ? "created=" + created + ", " : "")
+                + (lastChecked != null ? "lastChecked=" + lastChecked : "") + "]";
+    }
+}
