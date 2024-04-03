4e8d054ec1581eaed59c60c64dd604d70073634d
Vincent Privat <vincent.privat@gmail.com>
2024-04-04 01:44:37 +0200

    Scan NASA YouTube channels

---
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/NasaYouTubeService.java
@@ -0,0 +1,51 @@
+package org.wikimedia.commons.donvip.spacemedia.service.orgs;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.base.FileMetadata;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.youtube.YouTubeMedia;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.youtube.YouTubeMediaRepository;
+import org.wikimedia.commons.donvip.spacemedia.utils.Emojis;
+
+@Service
+public class NasaYouTubeService extends AbstractOrgYouTubeService {
+
+    public NasaYouTubeService(YouTubeMediaRepository repository,
+            @Value("${nasa.youtube.channels}") Set<String> youtubeChannels) {
+        super(repository, "nasa.youtube", youtubeChannels);
+    }
+
+    @Override
+    public String getName() {
+        return "NASA (YouTube)";
+    }
+
+    @Override
+    public String getUiRepoId(String repoId) {
+        // Remove "nasa" from ids displayed in UI to make the long list fit in screen
+        return super.getUiRepoId(repoId).replaceAll("(?i)nasa[_-]?", "").replace("Video", "");
+    }
+
+    @Override
+    protected String getAuthor(YouTubeMedia media, FileMetadata metadata) {
+        return "NASA";
+    }
+
+    @Override
+    protected List<String> getOrgCategories() {
+        return List.of("Videos of NASA");
+    }
+
+    @Override
+    protected Set<String> getEmojis(YouTubeMedia uploadedMedia) {
+        return Set.of(Emojis.ROCKET);
+    }
+
+    @Override
+    protected Set<String> getTwitterAccounts(YouTubeMedia uploadedMedia) {
+        return Set.of("@NASA");
+    }
+}
