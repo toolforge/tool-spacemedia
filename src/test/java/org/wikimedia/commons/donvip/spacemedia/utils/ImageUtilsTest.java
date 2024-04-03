d6a14528aebdb8e33373418b8157a0d9b9d34609
Vincent Privat <vincent.privat@gmail.com>
2024-04-03 22:05:03 +0200

    Read/decode documents (pdf, ppt, pptm, pptx)

--- src/test/java/org/wikimedia/commons/donvip/spacemedia/utils/ImageUtilsTest.java
+++ src/test/java/org/wikimedia/commons/donvip/spacemedia/utils/ImageUtilsTest.java
@@ -20,7 +20,7 @@ class ImageUtilsTest {
             "https://upload.wikimedia.org/wikipedia/commons/7/78/Empty_200x1.jpg,200,1",
             "https://upload.wikimedia.org/wikipedia/commons/e/e2/Artemis_program_%28original_with_wordmark%29.svg,200,185" })
     void testReadImage(URL url, int width, int height) throws Exception {
-        BufferedImage bi = ImageUtils.readImage(url, true, true).image();
+        BufferedImage bi = (BufferedImage) MediaUtils.readFile(url, true, true).contents();
         assertNotNull(bi);
         try {
             assertEquals(width, bi.getWidth());
