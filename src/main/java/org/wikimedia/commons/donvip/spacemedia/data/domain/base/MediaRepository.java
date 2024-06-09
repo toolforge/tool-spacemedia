25867c009522240925267f10f6647fcbe91542af
Vincent Privat <vincent.privat@gmail.com>
2024-06-09 21:23:44 +0200

    NOAA Library: initial support

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/base/MediaRepository.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/base/MediaRepository.java
@@ -57,6 +57,14 @@ public interface MediaRepository<T extends Media> extends JpaRepository<T, Compo
     @Query("select count(distinct (m.id)) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames)")
     long countMissingInCommons();
 
+    /**
+     * Count files already uploaded to Wikimedia Commons.
+     *
+     * @return number of files already uploaded to Wikimedia Commons
+     */
+    @Query("select count(distinct (m.id)) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames)")
+    long countUploadedToCommons();
+
     // COUNT composite
 
     @Query("select count(*) from #{#entityName} m where m.id.repoId in ?1")
@@ -90,7 +98,7 @@ public interface MediaRepository<T extends Media> extends JpaRepository<T, Compo
      *
      * @return files not yet uploaded to Wikimedia Commons
      */
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames)")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommons();
 
     /**
@@ -100,23 +108,15 @@ public interface MediaRepository<T extends Media> extends JpaRepository<T, Compo
      *
      * @return files not yet uploaded to Wikimedia Commons
      */
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames)")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findMissingInCommons(Pageable page);
-
-    /**
-     * Count files already uploaded to Wikimedia Commons.
-     *
-     * @return number of files already uploaded to Wikimedia Commons
-     */
-    @Query("select count(distinct (m.id)) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames)")
-    long countUploadedToCommons();
 
     /**
      * Find files already uploaded to Wikimedia Commons.
      *
      * @return files already uploaded to Wikimedia Commons
      */
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames)")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findUploadedToCommons();
 
     /**
@@ -126,10 +126,10 @@ public interface MediaRepository<T extends Media> extends JpaRepository<T, Compo
      *
      * @return files already uploaded to Wikimedia Commons
      */
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames)")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findUploadedToCommons(Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where size (md.commonsFileNames) >= 2")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where size (md.commonsFileNames) >= 2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findDuplicateInCommons();
 
     List<T> findByMetadata_AssetUrl(URL imageUrl);
@@ -156,63 +156,63 @@ public interface MediaRepository<T extends Media> extends JpaRepository<T, Compo
 
     // FIND Composite
 
-    @Query("select m from #{#entityName} m where m.id.repoId in ?1 order by m.publicationDate desc")
+    @Query("select m from #{#entityName} m where m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Set<T> findAll(Set<String> repos);
 
-    @Query("select m from #{#entityName} m where m.id.repoId in ?1")
+    @Query("select m from #{#entityName} m where m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findAll(Set<String> repos, Pageable page);
 
-    @Query("select m from #{#entityName} m where m.id.repoId in ?1 order by m.publicationDate desc limit 1")
+    @Query("select m from #{#entityName} m where m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc limit 1")
     Optional<T> findFirst(Set<String> repos);
 
-    @Query("select m from #{#entityName} m where m.id.repoId in ?1 and m.id.mediaId not in ?2 order by m.publicationDate desc")
+    @Query("select m from #{#entityName} m where m.id.repoId in ?1 and m.id.mediaId not in ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Set<T> findNotIn(Set<String> repos, Set<String> mediaIds);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.ignored = true and m.id.repoId in ?1 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.ignored = true and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findByMetadata_IgnoredTrue(Set<String> repos);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.ignored = true and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.ignored = true and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findByMetadata_IgnoredTrue(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where size (md.commonsFileNames) >= 2 and m.id.repoId in ?1 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where size (md.commonsFileNames) >= 2 and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findDuplicateInCommons(Set<String> repos);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('bmp','jpg','jpeg','tif','tiff','png','webp','xcf','gif','svg','exr') and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('bmp','jpg','jpeg','tif','tiff','png','webp','xcf','gif','svg','exr') and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findMissingImagesInCommons(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('mp4','webm','ogv','mpeg','wmv','avi') and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('mp4','webm','ogv','mpeg','wmv','avi') and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findMissingVideosInCommons(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('pdf','stl','epub','ppt','pptm','pptx') and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and md.extension in ('pdf','stl','epub','ppt','pptm','pptx') and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findMissingDocumentsInCommons(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommons(Set<String> repos);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.id.mediaId not in ?2 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.id.mediaId not in ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommonsNotIn(Set<String> repos, Set<String> mediaIds);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findMissingInCommons(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationDate = ?2 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationDate = ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommonsByPublicationDate(Set<String> repos, LocalDate date);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationMonth = ?2 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationMonth = ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommonsByPublicationMonth(Set<String> repos, YearMonth month);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationYear = ?2 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.publicationYear = ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommonsByPublicationYear(Set<String> repos, Year year);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.title = ?2 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where (md.ignored is null or md.ignored = false) and not exists elements (md.commonsFileNames) and m.id.repoId in ?1 and m.title = ?2 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findMissingInCommonsByTitle(Set<String> repos, String title);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationDate desc")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     List<T> findUploadedToCommons(Set<String> repos);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where exists elements (md.commonsFileNames) and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findUploadedToCommons(Set<String> repos, Pageable page);
 
-    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.phash is not null and m.id.repoId in ?1")
+    @Query("select distinct(m) from #{#entityName} m join m.metadata md where md.phash is not null and m.id.repoId in ?1 order by m.publicationYear desc, m.publicationMonth desc, m.publicationDate desc")
     Page<T> findByMetadata_PhashNotNull(Set<String> repos, Pageable page);
 }
