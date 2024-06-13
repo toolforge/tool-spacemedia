1ad08cba5bb0f40ce5c95bee286a01dc5ac7fac2
Vincent Privat <vincent.privat@gmail.com>
2024-06-14 01:04:09 +0200

    NASA Photojournal: drop Solr implementation (dead), switch to html gallery scrapping

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/nasa/photojournal/NasaPhotojournalMedia.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/nasa/photojournal/NasaPhotojournalMedia.java
@@ -1,26 +1,17 @@
 package org.wikimedia.commons.donvip.spacemedia.data.domain.nasa.photojournal;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.wikimedia.commons.donvip.spacemedia.data.domain.base.FileMetadata;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.base.Media;
-import org.wikimedia.commons.donvip.spacemedia.data.domain.base.WithKeywords;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Lob;
 
 @Entity
-public class NasaPhotojournalMedia extends Media implements WithKeywords {
+public class NasaPhotojournalMedia extends Media {
 
     private static final Pattern FIGURE = Pattern.compile("PIA\\d+_fig[^\\.]+\\..+", Pattern.CASE_INSENSITIVE);
-
-    @Column(name = "nasa_id", nullable = true, length = 48)
-    private String nasaId;
 
     @Column(name = "target", nullable = true, length = 50)
     private String target;
@@ -36,24 +27,6 @@ public class NasaPhotojournalMedia extends Media implements WithKeywords {
 
     @Column(name = "producer", nullable = true, length = 64)
     private String producer;
-
-    @Column(name = "big")
-    private boolean big;
-
-    @Lob
-    @Column(name = "legend", nullable = true, columnDefinition = "TEXT")
-    private String legend;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<String> keywords = new HashSet<>();
-
-    public String getNasaId() {
-        return nasaId;
-    }
-
-    public void setNasaId(String nasaId) {
-        this.nasaId = nasaId;
-    }
 
     public String getTarget() {
         return target;
@@ -96,32 +69,6 @@ public class NasaPhotojournalMedia extends Media implements WithKeywords {
     }
 
     @Override
-    public Set<String> getKeywords() {
-        return keywords;
-    }
-
-    @Override
-    public void setKeywords(Set<String> keywords) {
-        this.keywords = keywords;
-    }
-
-    public boolean isBig() {
-        return big;
-    }
-
-    public void setBig(boolean big) {
-        this.big = big;
-    }
-
-    public String getLegend() {
-        return legend;
-    }
-
-    public void setLegend(String legend) {
-        this.legend = legend;
-    }
-
-    @Override
     public String getUploadId(FileMetadata fileMetadata) {
         String file = fileMetadata.getAssetUrl().getFile();
         if (file.contains("/")) {
@@ -134,7 +81,7 @@ public class NasaPhotojournalMedia extends Media implements WithKeywords {
 
     @Override
     public String toString() {
-        return "NasaPhotojournalMedia [id=" + getId() + ", nasaId=" + nasaId + ", "
+        return "NasaPhotojournalMedia [id=" + getId() + ", "
                 + (target != null ? "target=" + target + ", " : "")
                 + (mission != null ? "mission=" + mission + ", " : "")
                 + (spacecraft != null ? "spacecraft=" + spacecraft + ", " : "")
@@ -144,14 +91,11 @@ public class NasaPhotojournalMedia extends Media implements WithKeywords {
 
     public NasaPhotojournalMedia copyDataFrom(NasaPhotojournalMedia mediaFromApi) {
         super.copyDataFrom(mediaFromApi);
-        setNasaId(mediaFromApi.getNasaId());
         setTarget(mediaFromApi.getTarget());
         setMission(mediaFromApi.getMission());
         setSpacecraft(mediaFromApi.getSpacecraft());
         setInstrument(mediaFromApi.getInstrument());
         setProducer(mediaFromApi.getProducer());
-        setBig(mediaFromApi.isBig());
-        setLegend(mediaFromApi.getLegend());
         return this;
     }
 }
--- src/test/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/NasaPhotojournalServiceTest.java
+++ src/test/java/org/wikimedia/commons/donvip/spacemedia/service/orgs/NasaPhotojournalServiceTest.java
@@ -1,24 +1,21 @@
 package org.wikimedia.commons.donvip.spacemedia.service.orgs;
 
+import static java.util.stream.Collectors.toSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.solr.client.solrj.response.QueryResponse;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,6 +24,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.geo.Point;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.base.CompositeMediaId;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.base.FileMetadata;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.nasa.photojournal.NasaPhotojournalMedia;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.nasa.photojournal.NasaPhotojournalMediaRepository;
@@ -34,8 +32,8 @@ import org.wikimedia.commons.donvip.spacemedia.exception.UploadException;
 import org.wikimedia.commons.donvip.spacemedia.service.CategorizationService;
 import org.wikimedia.commons.donvip.spacemedia.service.MediaService.MediaUpdateResult;
 import org.wikimedia.commons.donvip.spacemedia.service.nasa.NasaMappingService;
-import org.wikimedia.commons.donvip.spacemedia.service.orgs.NasaPhotojournalService.XMLResponseWithoutContentTypeParser;
 import org.wikimedia.commons.donvip.spacemedia.service.wikimedia.SdcStatements;
+import org.wikimedia.commons.donvip.spacemedia.utils.Utils;
 
 @SpringJUnitConfig(NasaPhotojournalServiceTest.TestConfig.class)
 class NasaPhotojournalServiceTest extends AbstractOrgServiceTest {
@@ -67,32 +65,24 @@ class NasaPhotojournalServiceTest extends AbstractOrgServiceTest {
     }
 
     @Test
-    void testSolrClient() {
-        assertNotNull(NasaPhotojournalService.solrClient("https://photojournal.jpl.nasa.gov/beta/solr/photojournal"));
-    }
+    void testReadHtm1() throws Exception {
+        NasaPhotojournalMedia media = readHtm("PIA25927");
 
-    @Test
-    void testReadXml1() throws Exception {
-        NasaPhotojournalMedia media = readXml("PIA25927");
-
-        assertEquals("JPL-20230509-PIA25927-ODYSSEY", media.getNasaId());
-        assertEquals("2023-05-09T13:19:04Z", media.getPublicationDateTime().toString());
         assertEquals("Mars", media.getTarget());
         assertEquals("2001 Mars Odyssey", media.getMission());
         assertEquals("2001 Mars Odyssey", media.getSpacecraft());
-        assertEquals("Thermal Emission Imaging System", media.getInstrument());
+        assertEquals("THEMIS", media.getInstrument());
         assertEquals("Arizona State University", media.getProducer());
-        assertEquals(Set.of(), media.getKeywords());
         assertEquals("NASA/JPL-Caltech/ASU", media.getCredits());
-        assertFalse(media.isBig());
         assertNotNull(media.getMetadata());
         assertEquals(3, media.getMetadata().size());
-        assertEquals(List.of("https://photojournal.jpl.nasa.gov/jpeg/PIA25927.jpg",
+        assertEquals(Set.of("https://photojournal.jpl.nasa.gov/jpeg/PIA25927.jpg",
                 "https://photojournal.jpl.nasa.gov/tiff/PIA25927.tif",
                 "https://photojournal.jpl.nasa.gov/figures/PIA25927_fig1.png"),
-                media.getMetadata().stream().map(m -> m.getAssetUrl().toExternalForm()).toList());
+                media.getMetadata().stream().map(m -> m.getAssetUrl().toExternalForm()).collect(toSet()));
 
-        FileMetadata jpeg = media.getMetadata().iterator().next();
+        FileMetadata jpeg = media.getMetadata().stream().filter(fm -> "jpg".equals(fm.getExtension())).findFirst()
+                .orElseThrow();
         assertEquals(
                 Optional.of("Kasei Valles (PIA25927).tiff|TIFF version\nKasei Valles (PIA25927_fig1).png|PNG version"),
                 service.getOtherVersions(media, jpeg));
@@ -104,8 +94,8 @@ class NasaPhotojournalServiceTest extends AbstractOrgServiceTest {
         assertEquals(Pair.of("Q207164", null), statements.get("P170"));
 
         assertEquals(
-                "<p><center>[[File:Kasei Valles (PIA25927_fig1).png|120px]]<br><b>Context image</b></center></p><p>Today's VIS image shows a portion of Kasei Valles. Kasei Valles is one of the largest outflow channel systems on Mars, in places up to 482 km (300 miles) wide and 1580 km (982 miles) long. For comparison, the Grand Canyon in Arizona is is only 29 km (18 miles) at its widest and only 446 km (277 miles) long. Kasei Valles flows eastward through Lunae Planum and empties into Chryse Planitia.</p><p>Orbit Number: 93928 Latitude: 25.9137 Longitude: 289.462 Instrument: VIS Captured: 2023-02-16 08:13</p><p>Please see the <a href=\"https://themis.asu.edu/terms\" target=\"_new\">THEMIS Data Citation Note</a> for details on crediting THEMIS images.</p><p>NASA's Jet Propulsion Laboratory manages the 2001 Mars Odyssey mission for NASA's Science Mission Directorate, Washington, D.C. The Thermal Emission Imaging System (THEMIS) was developed by Arizona State University, Tempe, in collaboration with Raytheon Santa Barbara Remote Sensing. The THEMIS investigation is led by Dr. Philip Christensen at Arizona State University. Lockheed Martin Astronautics, Denver, is the prime contractor for the Odyssey project, and developed and built the orbiter. Mission operations are conducted jointly from Lockheed Martin and from JPL, a division of the California Institute of Technology in Pasadena.</p>",
-                service.getDescription(media, jpeg));
+                "<p></p><center> [[File:Kasei Valles (PIA25927_fig1).png|120px]] <br><b>Context image</b></center><p></p><p>Today's VIS image shows a portion of Kasei Valles. Kasei Valles is one of the largest outflow channel systems on Mars, in places up to 482 km (300 miles) wide and 1580 km (982 miles) long. For comparison, the Grand Canyon in Arizona is is only 29 km (18 miles) at its widest and only 446 km (277 miles) long. Kasei Valles flows eastward through Lunae Planum and empties into Chryse Planitia.</p><p>Orbit Number: 93928 Latitude: 25.9137 Longitude: 289.462 Instrument: VIS Captured: 2023-02-16 08:13</p><p>Please see the <a href=\"https://themis.asu.edu/terms\" target=\"_new\">THEMIS Data Citation Note</a> for details on crediting THEMIS images.</p><p>NASA's Jet Propulsion Laboratory manages the 2001 Mars Odyssey mission for NASA's Science Mission Directorate, Washington, D.C. The Thermal Emission Imaging System (THEMIS) was developed by Arizona State University, Tempe, in collaboration with Raytheon Santa Barbara Remote Sensing. The THEMIS investigation is led by Dr. Philip Christensen at Arizona State University. Lockheed Martin Astronautics, Denver, is the prime contractor for the Odyssey project, and developed and built the orbiter. Mission operations are conducted jointly from Lockheed Martin and from JPL, a division of the California Institute of Technology in Pasadena.</p>",
+                service.getDescription(media, jpeg).replace("\n", ""));
 
         assertEquals(
                 Set.of("NASA Photojournal entries from 2023|PIA25927", "Photos of Mars by 2001 Mars Odyssey",
@@ -114,45 +104,60 @@ class NasaPhotojournalServiceTest extends AbstractOrgServiceTest {
     }
 
     @Test
-    void testReadXml2() throws Exception {
-        NasaPhotojournalMedia media = readXml("PIA25889");
+    void testReadHtm2() throws Exception {
+        NasaPhotojournalMedia media = readHtm("PIA25889");
 
         assertEquals(7, media.getMetadata().size());
         assertEquals(
-                List.of("https://photojournal.jpl.nasa.gov/jpeg/PIA25889.jpg",
+                Set.of("https://photojournal.jpl.nasa.gov/jpeg/PIA25889.jpg",
                         "https://photojournal.jpl.nasa.gov/tiff/PIA25889.tif",
                         "https://photojournal.jpl.nasa.gov/figures/PIA25889_figA.jpg",
                         "https://photojournal.jpl.nasa.gov/figures/PIA25889_figB.jpg",
                         "https://photojournal.jpl.nasa.gov/archive/PIA25889_MAIN_fullres.jpg",
                         "https://photojournal.jpl.nasa.gov/archive/PIA25889_FIGA_fullres.jpg",
                         "https://photojournal.jpl.nasa.gov/archive/PIA25889_FIGB_fullres.jpg"),
-                media.getMetadata().stream().map(m -> m.getAssetUrl().toExternalForm()).toList());
+                media.getMetadata().stream().map(m -> m.getAssetUrl().toExternalForm()).collect(toSet()));
 
         assertEquals(List.of("PIA25889", "PIA25889", "PIA25889_figA", "PIA25889_figB", "PIA25889_MAIN_fullres",
                 "PIA25889_figA", "PIA25889_figB"),
                 media.getMetadata().stream().map(m -> media.getUploadId(m)).toList());
     }
 
-    private NasaPhotojournalMedia readXml(String id) throws IOException, UploadException {
+    @Test
+    void testReadHtm3() throws Exception {
+        NasaPhotojournalMedia media = readHtm("PIA19688");
+
+        assertEquals(3, media.getMetadata().size());
+        assertEquals(
+                Set.of("https://photojournal.jpl.nasa.gov/tiff/PIA19688.tif",
+                        "https://photojournal.jpl.nasa.gov/archive/PIA19688_loop_bc_final.gif",
+                        "https://photojournal.jpl.nasa.gov/jpeg/PIA19688.jpg"),
+                media.getMetadata().stream().map(m -> m.getAssetUrl().toExternalForm()).collect(toSet()));
+
+        assertEquals(
+                List.of("PIA19688", "PIA19688", "PIA19688"),
+                media.getMetadata().stream().map(m -> media.getUploadId(m)).toList());
+    }
+
+    private NasaPhotojournalMedia readHtm(String id) throws IOException, UploadException {
         when(mediaService.updateMedia(any(), any(), any(), anyBoolean(), any(), anyBoolean(), anyBoolean(),
                 anyBoolean(), any())).thenReturn(new MediaUpdateResult(true, null));
         when(repository.save(any(NasaPhotojournalMedia.class))).thenAnswer(a -> a.getArgument(0, NasaPhotojournalMedia.class));
         when(metadataRepository.save(any(FileMetadata.class))).thenAnswer(a -> a.getArgument(0, FileMetadata.class));
 
-        List<NasaPhotojournalMedia> medias = service.processResponse(solrResponse(id));
+        NasaPhotojournalMedia media = new NasaPhotojournalMedia();
+        media.setId(new CompositeMediaId("photojournal", id));
+        Document html = html("nasa/photojournal/" + id + ".htm");
+        media.setPublication(
+                Utils.extractDate(html.getElementsByTag("dd").last().text(),
+                        List.of(DateTimeFormatter.ISO_LOCAL_DATE)).orElseThrow());
+        List<NasaPhotojournalMedia> medias = service.fillMediaWithHtml(null, html, null, media);
 
         assertEquals(1, medias.size());
-        NasaPhotojournalMedia media = medias.get(0);
+        assertEquals(media, medias.get(0));
         assertNotNull(media);
         assertEquals(id, media.getId().getMediaId());
         return media;
-    }
-
-    private static QueryResponse solrResponse(String id) throws IOException {
-        try (Reader reader = Files.newBufferedReader(Paths.get("src/test/resources/nasa/photojournal", id + ".xml"),
-                StandardCharsets.UTF_8)) {
-            return new QueryResponse(new XMLResponseWithoutContentTypeParser().processResponse(reader), null);
-        }
     }
 
     @Configuration
