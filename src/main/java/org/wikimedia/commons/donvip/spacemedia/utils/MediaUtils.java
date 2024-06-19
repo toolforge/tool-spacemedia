d1643e963ddbef936216679aa10940fdac865879
Vincent Privat <vincent.privat@gmail.com>
2024-06-19 02:07:40 +0200

    Fix reading of webm videos

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/utils/MediaUtils.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/utils/MediaUtils.java
@@ -48,6 +48,7 @@ import org.mp4parser.boxes.iso14496.part12.MovieBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wikimedia.commons.donvip.spacemedia.data.domain.base.FileMetadata;
+import org.wikimedia.commons.donvip.spacemedia.data.domain.base.ImageDimensions;
 import org.wikimedia.commons.donvip.spacemedia.exception.FileDecodingException;
 
 public class MediaUtils {
@@ -116,21 +117,26 @@ public class MediaUtils {
                     LOGGER.error("Image I/O error while reading {}: {}", uri, e.getMessage());
                     return new ContentsAndMetadata<>(null, contentLength.getAsLong(), filename, extension, 1, e);
                 }
-            } else if ("ogv".equals(extension)) {
-                return (ContentsAndMetadata<T>) readOgvVideo(in, contentLength.getAsLong(), filename, extension, uri);
             } else if ("www.youtube.com".equals(uri.getHost())) {
                 return (ContentsAndMetadata<T>) readMp4Video(localPath, contentLength, filename, extension, uri,
                         x -> ofNullable(downloadYoutubeVideo(x.toString())));
             } else if (FileMetadata.VIDEO_EXTENSIONS.contains(extension)) {
-                Path tempFile = Files.createTempFile("sm", "." + extension);
-                return (ContentsAndMetadata<T>) readMp4Video(localPath, contentLength, filename, extension, uri, x -> {
+                String ext = extension;
+                Function<URI, Optional<Path>> dl = x -> {
                     try {
+                        Path tempFile = Files.createTempFile("sm", "." + ext);
                         FileUtils.copyInputStreamToFile(in, tempFile.toFile());
                         return ofNullable(tempFile);
                     } catch (IOException e) {
                         throw new UncheckedIOException(e);
                     }
-                });
+                };
+                return (ContentsAndMetadata<T>) switch (extension) {
+                case "mp4" -> readMp4Video(localPath, contentLength, filename, extension, uri, dl);
+                case "ogv" -> readOgvVideo(in, contentLength.getAsLong(), filename, extension, uri);
+                case "webm" -> readWebmVideo(localPath, contentLength, filename, extension, uri, dl);
+                default -> throw new UnsupportedOperationException("Unsupported video format: " + extension);
+                };
             } else if ("webp".equals(extension)) {
                 ContentsAndMetadata<BufferedImage> result = ImageUtils.readWebp(uri, readMetadata);
                 return (ContentsAndMetadata<T>) new ContentsAndMetadata<>(result.contents(),
@@ -189,20 +195,43 @@ public class MediaUtils {
         }
     }
 
+    private static <T> ContentsAndMetadata<T> readVideo(Path localPath, URI uri,
+            Function<URI, Optional<Path>> downloader, Function<Path, ContentsAndMetadata<T>> reader)
+            throws FileDecodingException, IOException {
+        try {
+            if (localPath != null) {
+                return reader.apply(localPath);
+            } else {
+                Path tempFile = downloader.apply(uri)
+                        .orElseThrow(() -> new FileDecodingException("Failed to download video from " + uri));
+                try {
+                    return reader.apply(tempFile);
+                } finally {
+                    Files.delete(tempFile);
+                }
+            }
+        } catch (UncheckedIOException e) {
+            if (e.getCause().getCause() instanceof FileDecodingException fde) {
+                throw fde;
+            }
+            throw e.getCause();
+        }
+    }
+
     private static ContentsAndMetadata<IsoFile> readMp4Video(Path localPath, LongSupplier contentLength,
             String filename, String extension, URI uri, Function<URI, Optional<Path>> downloader)
             throws FileDecodingException, IOException {
-        if (localPath != null) {
-            return readMp4Video(localPath, contentLength, filename, extension);
-        } else {
-            Path tempFile = downloader.apply(uri)
-                    .orElseThrow(() -> new FileDecodingException("Failed to download video from " + uri));
+        return readVideo(localPath, uri, downloader, x -> {
             try {
-                return readMp4Video(tempFile, contentLength, filename, extension);
-            } finally {
-                Files.delete(tempFile);
+                try {
+                    return readMp4Video(x, contentLength, filename, extension);
+                } catch (FileDecodingException e) {
+                    throw new IOException(e);
+                }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
-        }
+        });
     }
 
     private static ContentsAndMetadata<IsoFile> readMp4Video(Path path, LongSupplier contentLength, String filename,
@@ -225,6 +254,54 @@ public class MediaUtils {
             } else {
                 throw e;
             }
+        }
+    }
+
+    private static ContentsAndMetadata<ImageDimensions> readWebmVideo(Path localPath, LongSupplier contentLength,
+            String filename, String extension, URI uri, Function<URI, Optional<Path>> downloader)
+            throws FileDecodingException, IOException {
+        return readVideo(localPath, uri, downloader, x -> {
+            try {
+                try {
+                    return readWebmVideo(x, contentLength, filename, extension);
+                } catch (FileDecodingException e) {
+                    throw new IOException(e);
+                }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
+
+    private static ContentsAndMetadata<ImageDimensions> readWebmVideo(Path path, LongSupplier contentLength,
+            String filename, String extension) throws IOException, FileDecodingException {
+        try {
+            int width = 0;
+            int height = 0;
+            for (String line : Utils.execOutput(List.of("exiftool", path.toString()), 1, TimeUnit.MINUTES)
+                    .split("\n")) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    switch (parts[0].trim()) {
+                    case "Image Width":
+                        width = Integer.parseInt(parts[1].trim());
+                        break;
+                    case "Image Height":
+                        height = Integer.parseInt(parts[1].trim());
+                        break;
+                    }
+                }
+            }
+            if (width == 0 && height == 0) {
+                throw new FileDecodingException("Failed to open WEBM video from " + path);
+            }
+            return new ContentsAndMetadata<>(new ImageDimensions(width, height), contentLength.getAsLong(), filename,
+                    extension, 1, null);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IOException(e);
+        } catch (ExecutionException e) {
+            throw new IOException(e);
         }
     }
 
