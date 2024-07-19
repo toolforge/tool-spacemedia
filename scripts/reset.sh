86d3ba15dce31e17e8109c1ff6eba177f02f7ff4
Vincent Privat <vincent.privat@gmail.com>
2024-07-19 23:14:43 +0200

    main is now the main branch

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -6,7 +6,7 @@ toolforge jobs flush 2> /dev/null
 rm -f cron-29.* && \
 rm -f logs/spacemedia* && \
 rm -Rf spacemedia && \
-git clone https://gitlab.wikimedia.org/toolforge-repos/spacemedia.git --single-branch --branch=develop-0.5.x && \
+git clone https://gitlab.wikimedia.org/toolforge-repos/spacemedia.git --single-branch --branch=main && \
 ./upgrade.sh
 
 cd -
