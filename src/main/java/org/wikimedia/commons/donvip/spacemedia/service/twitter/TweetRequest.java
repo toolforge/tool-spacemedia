15f63e4f7dbfc90cf91ca85184354255ab0d4bd5
Vincent Privat <vincent.privat@gmail.com>
2024-07-19 23:46:21 +0200

    Twitter: attempt to fix status requests

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/service/twitter/TweetRequest.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/service/twitter/TweetRequest.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-record TweetRequest(TweetMedia media, String text) {
+record TweetRequest(String text, TweetMedia media) {
 
     static record TweetMedia(@JsonProperty("media_ids") List<String> mediaIds) {
     }
