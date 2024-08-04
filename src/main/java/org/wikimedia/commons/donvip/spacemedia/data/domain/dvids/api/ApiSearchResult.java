29eca4139b337e21555be49e084b4b7d196dd535
Vincent Privat <vincent.privat@gmail.com>
2024-08-04 02:32:01 +0200

    DVIDS: add robustness

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/dvids/api/ApiSearchResult.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/dvids/api/ApiSearchResult.java
@@ -1,11 +1,14 @@
 package org.wikimedia.commons.donvip.spacemedia.data.domain.dvids.api;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import org.wikimedia.commons.donvip.spacemedia.data.domain.base.CompositeMediaId;
 import org.wikimedia.commons.donvip.spacemedia.service.dvids.DvidsService;
 
 public record ApiSearchResult(String id, String unit_name) {
 
-    public CompositeMediaId toCompositeMediaId(DvidsService dvids) {
-        return new CompositeMediaId(dvids.getUnitAbbreviation(unit_name), id);
+    public CompositeMediaId toCompositeMediaId(String unit, DvidsService dvids) {
+        return new CompositeMediaId("*".equals(unit) || isBlank(unit) ? dvids.getUnitAbbreviation(unit_name) : unit,
+                id);
     }
 }
