c71d2db5bcd5c7f5af53b3ef1c3d22bf7347f05c
Don-vip <vincent.privat@gmail.com>
2024-08-18 21:11:47 +0200

    simplify caption table structure

--- src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/base/Caption.java
+++ src/main/java/org/wikimedia/commons/donvip/spacemedia/data/domain/base/Caption.java
@@ -1,27 +1,18 @@
 package org.wikimedia.commons.donvip.spacemedia.data.domain.base;
 
-import static jakarta.persistence.GenerationType.SEQUENCE;
-
 import java.net.URL;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
+@Embeddable
 @Table(indexes = { @Index(columnList = "url") })
 public class Caption {
-    
-    @Id
-    @JsonIgnore
-    @GeneratedValue(strategy = SEQUENCE, generator = "caption_sequence")
-    private Long id;
 
     @Column(nullable = false, length = 8)
     private String lang;
@@ -35,14 +26,6 @@ public class Caption {
     public Caption(String lang, URL url) {
         this.lang = lang;
         this.url = url;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getLang() {
@@ -63,7 +46,7 @@ public class Caption {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, lang, url);
+        return Objects.hash(lang, url);
     }
 
     @Override
@@ -73,11 +56,11 @@ public class Caption {
         if (obj == null || getClass() != obj.getClass())
             return false;
         Caption other = (Caption) obj;
-        return Objects.equals(id, other.id) && Objects.equals(lang, other.lang) && Objects.equals(url, other.url);
+        return Objects.equals(lang, other.lang) && Objects.equals(url, other.url);
     }
 
     @Override
     public String toString() {
-        return "Caption [id=" + id + ", lang=" + lang + ", url=" + url + "]";
+        return "Caption [lang=" + lang + ", url=" + url + "]";
     }
 }
