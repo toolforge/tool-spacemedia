eb1360f41bae1b93e5c73657e344c83f148d011c
Don-vip <vincent.privat@gmail.com>
2024-09-01 13:15:39 +0200

    fix NPE

--- src/test/java/org/wikimedia/commons/donvip/spacemedia/utils/UnitedStatesTest.java
+++ src/test/java/org/wikimedia/commons/donvip/spacemedia/utils/UnitedStatesTest.java
@@ -1,5 +1,6 @@
 package org.wikimedia.commons.donvip.spacemedia.utils;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,12 @@ class UnitedStatesTest {
     @Test
     void testIsVirin() {
         assertTrue(UnitedStates.isVirin("170831-F-HW403-077"));
+    }
+
+    @Test
+    void testGetUsVirinTemplates() {
+        assertNull(UnitedStates.getUsVirinTemplates(" ", ""));
+        assertNull(UnitedStates.getUsVirinTemplates("", ""));
+        assertNull(UnitedStates.getUsVirinTemplates(null, ""));
     }
 }
